412539ea82b594db928843d5b0357d83667209b7
taehoon95 <leeth950304@gmail.com>
2021-06-14 10:11:55 +0900

    산술연산

---
+++ 산술연산/dataframe_to_number.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import pandas as pd
+
+titanic = sns.load_dataset("titanic")
+
+pd.set_option('display.max_columns', len(titanic.columns))
+pd.set_option('display.width', 1000)
+
+print('# titanic 데이터 셋', type(titanic),titanic.head(), sep='\n', end='\n\n')
+
+print('# titanic 데이터셋에서 age, fare 2개 열을 선택하여 데이터프레임 만들기')
+df = titanic.loc[:, ['age', 'fare']]
+print(df.head(), type(df), sep='\n', end='\n\n')
+
+print('# 데이터프레임에 숫자 10 더하기')
+addtion = df + 10
+print(addtion.head(), type(addtion), sep='\n')
